5ffd4f9401bb90393a9beb296e768ad5c8756390
Arodin <62658844+OdArthur@users.noreply.github.com>
2022-11-07 19:34:54 -0500

    Adding Createproject page

---
+++ public/createprojet.php
@@ -0,0 +1,11 @@
+<?php
+// ~/php/tp1/public/main.php
+// include database
+// include __DIR__ . '/../database/db.php';
+// include model
+// include __DIR__ . '/../model/games.php';
+// include view
+// include(__DIR__ . '/../view/top.html');
+include(__DIR__ . '/../Main/cssimport.php');
+include(__DIR__ . '/../Main/createprojet.php');
+include(__DIR__ . '/../Main/jsimport.php');
