45cb99754d91274b7e4cf911180a5475f6ca28a7
Yrthal <baptiste.comby.pro@gmail.com>
2022-11-17 14:56:53 +0100

    Add : Page all your projects et all projects pour une visualisation plus clair

---
+++ view/AllItems/AllYourProjects.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!-- saved from url=(0056)https://colorlib.com/etc/bootstrap-sidebar/sidebar-03/?# -->
+<html lang="en" data-lt-installed="true">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>OCC</title>
+    <link rel="shortcut icon" href="../Assets/IconOCC.png" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
+    <div class="wrapper d-flex align-items-stretch">
+
+        <?php
+        include __DIR__ . '/../Sidebar.php';
+        ?>
+
+        <div id="content" class="p-4 p-md-5 pt-5">
+            <h2 class="mb-4">All Your Projects</h2>
+            
+        </div>
+    </div>
+</body>
+
+</html>
